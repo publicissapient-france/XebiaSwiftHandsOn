3c3a81641abbecc33bcce03757de574823493479
Simone Civetta <simonecivetta@gmail.com>
2014-06-18 01:18:37 +0200

    Adding optional playground

--- Playgrounds/05_Optional.playground/section-1.swift
+++ Playgrounds/05_Optional.playground/section-1.swift
@@ -1,7 +1,54 @@
 /************************
-Optional
-************************/
+ * Optional
+ ***********************/
 
+// Optional
+var optionalString: String? = "Hello"
+optionalString == nil
 
+// Unwrapping
+var optionalStringUnwrapped = optionalString!
 
+var emtpyName: String? = nil
+var emptyNameUnwrapped = emtpyString! // Error!
 
+// Unwrapping
+if optionalString {
+    var unwrapped = optionalString!
+    var greeting = "\(optionalString), Swifter!"
+}
+
+// Unwrapping, fast
+if let optionalStringUnwrapped = optionalString {
+    var greeting = "\(optionalString), Swifter!"
+}
+
+// Chaining
+class Person {
+    var name:String?
+    
+    init(name:String) {
+        self.name = name
+    }
+}
+
+class Movie {
+    var title:String?
+    var director:Person?
+    
+    init(title:String, director:Person) {
+        self.title = title
+        self.director = director
+    }
+}
+
+var pulpFiction:Movie?
+pulpFiction =
+    Movie(
+        title: "Pulp Fiction",
+        director: Person(name: "Quentin Tarantino")
+    )
+
+if let directorName = pulpFiction?.director?.name {
+    println(directorName)
+}
