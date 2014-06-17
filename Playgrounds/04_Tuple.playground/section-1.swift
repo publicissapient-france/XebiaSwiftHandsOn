8de80ce24658894f94fbf30b06ee7fe86670dbdc
Simone Civetta <simonecivetta@gmail.com>
2014-06-17 18:12:37 +0200

    Tuple playground

--- Playgrounds/04_Tuple.playground/section-1.swift
+++ Playgrounds/04_Tuple.playground/section-1.swift
@@ -10,8 +10,9 @@ unrelated type, "mini-struct"
 let c = 6
 
 ()          // empty tuple (void)
-(c) == c    // one element tuple are juste simple value
-(c,c)       // "normal" tuple
+(c) == c    // one element tuple is just a simple value
+(c,c)       // two-element tuple
+(c,c,c)     // three-element tuple
 
 // accessing elements
 var tup1 = (5,6)
@@ -23,11 +24,8 @@ var tup2: (a: Int, b: Int) = (5,6)
 tup2.a
 tup2.b
 
-// idem for function params
-func ft(a: (Int, Int)) -> Int {
-    return a.0 + a.1
+// Function paramemeters
+func sayHello(params: (guy1:String, guy2:String)) -> String {
+    return "Hello \(params.guy1) & \(params.guy2)"
 }
-
-func ft2(a: (b1: Int, b2: Int)) -> Int {
-    return a.b1 + a.b2
-}
+sayHello(("Stan", "Laurel"))
